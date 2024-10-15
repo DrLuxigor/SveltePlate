8337f0bc9dcfbe1040592949d837a1c8e2264658
Lukas Kofler <lukaskofler@hotmail.de>
2024-10-16 01:14:54 +0200

    commented old microsoft auth

--- src/routes/auth/+server.ts
+++ src/routes/auth/+server.ts
@@ -1,8 +1,8 @@
-import { ConfidentialClientApplication, CryptoProvider } from "@azure/msal-node";
-import { redirect } from '@sveltejs/kit';
-import { CLOUD_INSTANCE, TENANT_ID, CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, POST_LOGOUT_REDIRECT_URI} from '$env/static/private';
+//import { ConfidentialClientApplication, CryptoProvider } from "@azure/msal-node";
+//import { redirect } from '@sveltejs/kit';
+//import { CLOUD_INSTANCE, TENANT_ID, CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, POST_LOGOUT_REDIRECT_URI} from '$env/static/private';
 
-const msalConfig = {
+/*const msalConfig = {
     auth: {
         clientId: CLIENT_ID, // 'Application (client) ID' of app registration in Azure portal - this value is a GUID
         authority: CLOUD_INSTANCE + TENANT_ID, // Full directory URL, in the form of https://login.microsoftonline.com/<tenant>
@@ -23,17 +23,17 @@ const instance = getMsalInstance(msalConfig);
 
 function getMsalInstance(msalConfig:any) {
     return new ConfidentialClientApplication(msalConfig);
-}
+}*/
 
 
 
 /** @type {import('./$types').RequestHandler} */
-export async function GET({ url }:any) {
+/*export async function GET({ url }:any) {
     const red = await instance.getAuthCodeUrl({
         redirectUri: REDIRECT_URI,
         scopes: ['user.read'],
     });
 
 	return redirect(302, red)
-}
+}*/
 
