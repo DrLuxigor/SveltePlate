d38238c22032ce11caed4e00f9f5c1d9558d34a5
Lukas Kofler <lukaskofler@hotmail.de>
2024-10-20 08:10:08 +0200

    trying again 2

--- src/routes/paster/[content]/+page.server.ts
+++ src/routes/paster/[content]/+page.server.ts
@@ -2,6 +2,7 @@ import { error } from '@sveltejs/kit';
 
 /** @type {import('./$types').PageLoad} */
 export function load({ params }: any) {
+    console.log("content: " + params.content);
 	return {
         content: params.content
     }
